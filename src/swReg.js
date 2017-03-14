042c967ec8dad558389725bcbf7c8c45ef2a1645
devarsh shah <devarshmshah@gmail.com>
2017-03-14 17:15:23 +0530

    Project Bootstrapping

---
+++ src/swReg.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js').then(reg => {
+    console.log(`Registration succeeded. Scope is ${reg.scope}`);
+  }).catch(err => {
+    console.log(`Registration failed with ${err.toString()}`);
+  });
+}
